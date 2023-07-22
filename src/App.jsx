1b9b297daa1bae5c6a1cefb2950f4fb3323b5b3b
daniel7842900 <kyungmin.dan.lim@gmail.com>
2023-07-21 17:11:08 -0700

    initial commit with basic html

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="greetings-container">
+        <h1>Welcome to Quote Generator</h1>
+      </div>
+      <div>
+        <span>main quote here</span>
+      </div>
+      <div>
+        <button>button</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
