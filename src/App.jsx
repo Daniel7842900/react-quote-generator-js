9bda85342ea96eea56298f3572017e628a260d56
daniel7842900 <kyungmin.dan.lim@gmail.com>
2023-07-23 11:53:10 -0700

    add html and css w tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <div className="greetings-container">
-        <h1>Welcome to Quote Generator</h1>
+    <div className="min-h-screen h-screen m-0 bg-stone-100">
+      <div className="flex h-1/5  justify-center items-center mx-auto">
+        <h1 className="text-5xl">Welcome to Quote Generator</h1>
       </div>
-      <div>
-        <span>main quote here</span>
+      <div className="flex h-3/5 justify-center items-center mx-auto">
+        <span className="text-6xl italic">main quote here</span>
       </div>
-      <div>
-        <button>button</button>
+      <div className="flex h-1/5 justify-center mx-auto">
+        <div>
+          <button className="text-4xl rounded-full py-1 px-3 border-2 border-black">
+            Generate Quote
+          </button>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
