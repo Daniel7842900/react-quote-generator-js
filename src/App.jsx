6f92aaa242a8b10505f22d8ac55fb751fa40c032
daniel7842900 <kyungmin.dan.lim@gmail.com>
2023-07-23 14:43:36 -0700

    add chatgpt set up and configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
+import { useEffect } from "react";
+import { Configuration, OpenAIApi } from "openai";
 import "./App.css";
 
 function App() {
+  const configuration = new Configuration({
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  useEffect(() => {
+    makeRequest();
+  }, []);
+
+  const makeRequest = async () => {
+    const chat_completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: "Give me 10 inspiring quotes." }],
+    });
+  };
+
   return (
     <div className="min-h-screen h-screen m-0 bg-stone-100">
       <div className="flex h-1/5  justify-center items-center mx-auto">
