24fc2bb801952ea448205229c537c64adb8db2ae
daniel7842900 <kyungmin.dan.lim@gmail.com>
2023-07-26 21:37:34 -0700

    refactor fetching methods to custom hook.

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,22 @@
-import { useState } from "react";
-import Apiclient from "./services/ApiClient";
+import useData from "./hooks/useData";
 import Loader from "./components/Loader";
 import "./App.css";
 
 function App() {
-  const [response, setResponse] = useState();
-  const [quoteItems, setQuoteItems] = useState([]);
-  const [currentQuote, setCurrentQuote] = useState({});
-  const [isLoading, setLoading] = useState(false);
-
-  /**
-   * Send a request to OpenAI API and fetch the data with the given prompt.
-   *
-   * @returns an array of objects
-   */
-  const makeRequest = async () => {
-    setLoading(true);
-
-    const PROMPT = "Give me 10 inspiring quotes only.";
-
-    // Construct the option that will be used to send a request
-    const completedOptions = Apiclient.constructOptions(PROMPT);
-
-    // Make a call with given option
-    const openai = Apiclient.configureOpenAI();
-
-    const chat_completion = await openai.createChatCompletion(completedOptions);
-
-    // Store the response (only the content)
-    const responseContent = chat_completion.data.choices[0].message.content;
-    const quotes = responseContent.split("\n");
-    const quoteItems = [];
-
-    quotes.forEach((q) => {
-      // Capture the number from the string
-      const numberMatchId = q.match(/^\d+/);
-      const id = numberMatchId ? parseInt(numberMatchId[0]) : null;
-
-      // Capture the content quote from the string
-      const content = q.slice(3);
-
-      // Construct an object with captured info
-      const quote = {
-        id: id,
-        content: content,
-        displayed: false,
-      };
-
-      // Add a quote object into an array
-      quoteItems.push(quote);
-    });
-
-    setLoading(false);
-    setResponse(responseContent);
-    setQuoteItems(quoteItems);
-    return quoteItems;
-  };
+  const { makeRequest, findNextQuote, quoteItems, currentQuote, isLoading } =
+    useData();
 
   /**
    * Handles Generate Quote button click.
    */
+  // TODO: deal with when the last item has reached.
   const handleOnclick = async () => {
     // Check if quoteItems is empty or not
     if (quoteItems.length > 0) {
       // Check if the last quote is already displayed.
       // If so, fetch another quotes and display the first one.
       if (quoteItems[quoteItems.length - 1]["displayed"] === true) {
-        setQuoteItems([]);
+        // setQuoteItems([]);
 
         const fetchedData = await makeRequest();
         await findNextQuote(fetchedData);
@@ -76,29 +26,6 @@ function App() {
     } else {
       const fetchedData = await makeRequest();
       await findNextQuote(fetchedData);
-    }
-  };
-
-  /**
-   * Find the next quote to render
-   * Find the quote that has displayed property false.
-   * If the quote is found, update the states accordingly.
-   *
-   * @param {*} quoteItems
-   */
-  const findNextQuote = async (quoteItems) => {
-    // Find the quote hasn't displayed yet
-    const itemToDisplay = quoteItems.find((item) => item.displayed === false);
-
-    // If found, set the states accordingly
-    if (itemToDisplay !== undefined) {
-      const updatedItem = { ...itemToDisplay, displayed: true };
-      const updatedQuoteItems = quoteItems.map((item) =>
-        item.id === itemToDisplay.id ? updatedItem : item
-      );
-
-      setCurrentQuote(updatedItem);
-      setQuoteItems(updatedQuoteItems);
     }
   };
 
